29e4fe174d539a03af9f7adfff1d0a6bb4b66cba
Thiago5ena <thiagosena.rj@gmail.com>
2018-12-13 19:47:25 -0200

    Removando comentários

--- main.js
+++ main.js
@@ -1,39 +1,3 @@
-// var MOCK_DA_RESPOSTA = {
-// 	"name": "Luke Skywalker Ferreira da Silva Orleans e Bragança",
-// 	"height": "172",
-// 	"mass": "77",
-// 	"hair_color": "blond",
-// 	"skin_color": "fair",
-// 	"eye_color": "blue",
-// 	"birth_year": "19BBY",
-// 	"gender": "male",
-// 	"homeworld": "https://swapi.co/api/planets/1/",
-// 	"films": [
-// 		"https://swapi.co/api/films/2/",
-// 		"https://swapi.co/api/films/6/",
-// 		"https://swapi.co/api/films/3/",
-// 		"https://swapi.co/api/films/1/",
-// 		"https://swapi.co/api/films/7/"
-// 	],
-// 	"species": [
-// 		"https://swapi.co/api/species/1/"
-// 	],
-// 	"vehicles": [
-// 		"https://swapi.co/api/vehicles/14/",
-// 		"https://swapi.co/api/vehicles/30/"
-// 	],
-// 	"starships": [
-// 		"https://swapi.co/api/starships/12/",
-// 		"https://swapi.co/api/starships/22/"
-// 	],
-// 	"created": "2014-12-09T13:50:51.644000Z",
-// 	"edited": "2014-12-20T21:17:56.891000Z",
-// 	"url": "https://swapi.co/api/people/1/"
-// }
-
-
-/* AJAX é uma técnica */
-
 function preencherPerfil(info) {
 	var nome = document.querySelector(".nome");
 	var altura = document.querySelector("#altura");
@@ -53,23 +17,6 @@ function preencherPerfil(info) {
 		)
 	})
 }
-
-/* API são elos entre o front e o back */
-
-
-// AJAX default
-// function loadDoc() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-// 		preencherPerfil(JSON.parse(this.responseText));
-//     }
-//   };
-//   xhttp.open("GET", "https://swapi.co/api/people/32/", true);
-//   xhttp.send();
-// }
-
-// loadDoc();
 
 
 // AJAX com jQuery
